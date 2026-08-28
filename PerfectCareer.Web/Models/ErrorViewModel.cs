9e9a5a2fbc942c8d7be8ac8de400aca0898926da
Yamin Badawi <yaminbadawi1@gmail.com>
2026-08-28 17:35:49 +0300

    Add project files.

---
+++ PerfectCareer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PerfectCareer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
